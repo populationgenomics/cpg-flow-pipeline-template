f79dc76ba97e60209e5bfdef693fe0d09ad4f017
MattWellie <mattwellie@gmail.com>
2025-06-25 10:30:28 +1000

    more renovations

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # cpg-flow-pipeline-template
 A template repository to use as a base for CPG workflows using the cpg-flow pipeline framework
 
+Current version: 0.1.0
+
 ## Purpose
 
 When migrating workflows from production-pipelines, this template respository structure can be used to start with a
--- src/workflow_name/jobs/PrintPreviousJobOutputInAPythonJob.py
+++ src/workflow_name/jobs/PrintPreviousJobOutputInAPythonJob.py
@@ -4,34 +4,52 @@ trivially simple python job example, using a utility constant
 
 from typing import TYPE_CHECKING
 
-from cpg_utils.config import config_retrieve
-from cpg_utils.hail_batch import get_batch
+from cpg_utils import Path, config, hail_batch
 
-from workflow_name.python_jobs.PrintPreviousJobOutputInAPythonJob import print_file_contents
+from workflow_name.utils import DATE_STRING
 
 if TYPE_CHECKING:
     from hailtop.batch.job import Job
 
 
-def set_up_printing_python_job(input_file: str, output_file: str) -> 'Job':
+def print_file_contents(input_file: str) -> str:
+    """
+    This is a simple example of a job that prints the contents of a file.
+
+    Args:
+        input_file (str): the path to the file to print
+    """
+
+    with open(input_file) as f:
+        contents = f.read()
+
+    print(f'Contents of {input_file} on {DATE_STRING}:')
+    print(contents)
+    return contents
+
+
+def set_up_printing_python_job(input_file: str, output_file: Path, job_attrs: dict[str, str]) -> 'Job':
     """
     This is a simple example of a job that prints the contents of a file.
     This is the logic for the stage, calling the pythonJob to do the work
 
     Args:
         input_file (str): the path to the file to print
-        output_file (str): the path to write the result to
+        output_file (Path): the path to write the result to
+        job_attrs (dict[str, str]): attributes to attach to the job
     """
 
+    batch_instance = hail_batch.get_batch()
+
     # localise the file
-    local_input = get_batch().read_input(input_file)
+    local_input = batch_instance.read_input(input_file)
 
     # run the PythonJob
-    job = get_batch().new_python_job(f'Read {input_file}')
-    job.image(config_retrieve(['workflow', 'driver_image']))
+    job = batch_instance.new_python_job(f'Read {input_file}', attributes=job_attrs)
+    job.image(config.config_retrieve(['workflow', 'driver_image']))
     pyjob_output = job.call(
         print_file_contents,
         local_input,
     )
-    get_batch().write_output(pyjob_output.as_str(), output_file)
+    batch_instance.write_output(pyjob_output.as_str(), output_file)
     return job
