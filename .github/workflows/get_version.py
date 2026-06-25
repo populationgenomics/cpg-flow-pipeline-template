4b1c235754d480d8f548e0dbfd282c1cbc18afb2
MattWellie <mattwellie@gmail.com>
2026-06-25 13:37:23 +1000

    update image deploy workflow

--- .github/workflows/get_version.py
+++ .github/workflows/get_version.py
@@ -1,5 +1,4 @@
 import json
-import logging
 import os
 import re
 import subprocess
@@ -27,54 +26,36 @@ def get_next_version_tag(folder: str, version: str) -> str:
         'GCP_BASE_IMAGE',
         'australia-southeast1-docker.pkg.dev/cpg-common/images',
     )
-    base_image_path_archive = os.environ.get(
-        'GCP_BASE_ARCHIVE_IMAGE',
-        'australia-southeast1-docker.pkg.dev/cpg-common/images-archive',
-    )
-    full_image_name_prod = f'{base_image_path_prod}/{folder}'
-    full_image_name_archive = f'{base_image_path_archive}/{folder}'
-
-    tags_list = []
-
-    logging.basicConfig(level=logging.ERROR)
-
-    for full_image_name in [full_image_name_prod, full_image_name_archive]:
-        cmd = [
-            'gcloud',
-            'container',
-            'images',
-            'list-tags',
-            full_image_name,
-            '--format=json',
-        ]
-        try:
-            result = subprocess.run(cmd, capture_output=True, text=True, check=True)  # noqa: S603
-        except subprocess.CalledProcessError:
-            print(f'Failed to list tags for {full_image_name}')
-            continue
-
-        # If existing tags are found, proceed to determine the next version.
-        tags_list += json.loads(result.stdout)
+    cmd = [
+        'gcloud',
+        'container',
+        'images',
+        'list-tags',
+        f'{base_image_path_prod}/{folder}',
+        '--format=json',
+    ]
 
     max_suffix = 0
     pattern = re.compile(rf'^{re.escape(version)}-(\d+)$')
 
-    # If no tags are found, it returns the next version as -1.
-    for entry in tags_list:
-        tags = entry.get('tags', [])
-        for tag in tags:
-            match = pattern.match(tag)
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)  # noqa: S603
+        for block in json.loads(result.stdout):
+            if not block.get('tags', []):
+                continue
+            match = pattern.match(block['tags'][0])
             if match:
                 num = int(match.group(1))
                 max_suffix = max(max_suffix, num)
-    new_suffix = max_suffix + 1
-    return f'{version}-{new_suffix}'
+    except subprocess.CalledProcessError as err:
+        raise RuntimeError('Failed to list tags for the given image') from err
+
+    return f'{version}-{max_suffix + 1}'
 
 
 def main():
     dockerfile_name = 'Dockerfile'
-    # TODO edit this image name
-    container_name = 'example_workflow_name'
+    container_name = 'cpg-flow-example'
     current_version = extract_version_from_file(dockerfile_name)
     if current_version is None:
         # Throw an error here
@@ -83,12 +64,8 @@ def main():
     # Determine the next available tag based on current_version.
     new_tag = get_next_version_tag(container_name, current_version)
 
-    include_entries = [{'name': container_name, 'tag': new_tag}]
-
-    # Build the final matrix structure.
-    matrix = {'include': include_entries}
-    print(json.dumps(matrix, separators=(',', ':')), file=sys.stderr)
-    print(json.dumps(matrix, separators=(',', ':')), end='')
+    print(new_tag, file=sys.stderr)
+    print(new_tag, end='')
 
 
 main()
